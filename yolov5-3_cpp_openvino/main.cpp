ecfffc10edbdc51bdc8a20cd4610304a4e3bae23
DYF-AI <dongyongfei36@gmail.com>
2021-04-11 23:42:10 +0800

    update

--- yolov5-3_cpp_openvino/main.cpp
+++ yolov5-3_cpp_openvino/main.cpp
@@ -5,26 +5,26 @@ int main(int argc, char const *argv[])
     Detector* detector = new Detector;
     string xml_path = "../weights/yolov5s.xml";
     detector->init(xml_path,0.1,0.5);
-    /*
-    VideoCapture capture;
-    capture.open(0);
-    Mat src;
-    while(1){
-        capture >> src;
-        vector<Detector::Object> detected_objects;
-    detector->process_frame(src,detected_objects);
-    for(int i=0;i<detected_objects.size();++i){
-         int xmin = detected_objects[i].rect.x;
-        int ymin = detected_objects[i].rect.y;
-        int width = detected_objects[i].rect.width;
-        int height = detected_objects[i].rect.height;
-        Rect rect(xmin, ymin, width, height);//左上坐标（x,y）和矩形的长(x)宽(y)
-        cv::rectangle(src, rect, Scalar(255, 0, 0),1, LINE_8,0);
-    }
-        imshow("cap",src);
-        waitKey(1);
-    }
-    */
+    // /*
+    // VideoCapture capture;
+    // capture.open(0);
+    // Mat src;
+    // while(1){
+    //     capture >> src;
+    //     vector<Detector::Object> detected_objects;
+    // detector->process_frame(src,detected_objects);
+    // for(int i=0;i<detected_objects.size();++i){
+    //      int xmin = detected_objects[i].rect.x;
+    //     int ymin = detected_objects[i].rect.y;
+    //     int width = detected_objects[i].rect.width;
+    //     int height = detected_objects[i].rect.height;
+    //     Rect rect(xmin, ymin, width, height);//左上坐标（x,y）和矩形的长(x)宽(y)
+    //     cv::rectangle(src, rect, Scalar(255, 0, 0),1, LINE_8,0);
+    // }
+    //     imshow("cap",src);
+    //     waitKey(1);
+    // }
+    // */
     Mat src = imread("../images/bus.jpg");
     Mat osrc = src.clone();
     resize(osrc,osrc,Size(640,640));
